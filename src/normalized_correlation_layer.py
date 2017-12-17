48e749ad85e9040e5de6e12d88a46a4553acf436
Sumedh Pendurkar <sumedh.pendurkar@gmail.com>
2017-12-18 02:32:19 +0530

    normalized_correlation_layer works on keras 2.0

--- src/normalized_correlation_layer.py
+++ src/normalized_correlation_layer.py
@@ -1,33 +1,14 @@
-from __future__ import absolute_import
-from __future__ import division
-
 import pdb
-import copy
-import inspect
-import warnings
 import numpy as np
-import tensorflow as tf
-import types as python_types
 from keras import backend as K
-from keras.regularizers import l2
-from keras.layers.convolutional import Convolution2D
-from keras.engine import InputSpec, Layer, Merge
+import keras
+from keras.engine import InputSpec, Layer
 from keras import regularizers
-from keras.utils.np_utils import conv_output_length
-from keras.utils.generic_utils import func_dump, func_load
-from keras.layers.recurrent import Recurrent, time_distributed_dense
-from keras import activations, initializations, regularizers, constraints
-from keras.layers import BatchNormalization
+from keras.utils.conv_utils import conv_output_length
+from keras import activations
 from keras.layers import (
      Input,
      Activation,
-     Merge,
-     merge,
-     Dropout,
-     Reshape,
-     Permute,
-     Dense,
-     UpSampling2D,
      Flatten,
      Lambda
     )
@@ -47,7 +28,6 @@ class Normalized_Correlation_Layer(Layer):
     do padding before giving the layer.
     
     '''
-
     def __init__(self, patch_size=5,
                  dim_ordering='tf',
                  border_mode='valid',
@@ -63,8 +43,8 @@ class Normalized_Correlation_Layer(Layer):
         self.border_mode = border_mode
         self.activation = activations.get(activation)
         super(Normalized_Correlation_Layer, self).__init__(**kwargs)
-
-    def get_output_shape_for(self, input_shape):
+        return
+    def get_output_shape(self, input_shape):
         if self.dim_ordering == 'tf':
             inp_rows = input_shape[0][1]
             inp_cols = input_shape[0][2]
@@ -79,12 +59,11 @@ class Normalized_Correlation_Layer(Layer):
         out_c = conv_output_length(inp_cols, self.kernel_size,
                                    self.border_mode, self.subsample[1])
         return (input_shape[0][0], rows, cols, out_r * out_c)
-
     def call(self, x, mask=None):
         input_1, input_2 = x
         stride_row, stride_col = self.subsample
         inp_shape = input_1._keras_shape
-        output_shape = self.get_output_shape_for([inp_shape, inp_shape])
+        output_shape = self.get_output_shape([inp_shape, inp_shape])
         output_row = inp_shape[1] - self.kernel_size + 1
         output_col = inp_shape[2] - self.kernel_size + 1
         xc_1 = []
